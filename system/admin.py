3885194a925a9014b05720f5dbb1c18c5f2e0c73
John Anthony Romeo <jaromeo65@gmail.com>
2026-05-07 22:45:17 +0800

    Deleted All

--- system/admin.py
+++
@@ -1,9 +0,0 @@
-# system/admin.py
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import User  
-
-@admin.register(User)
-class UserAdmin(BaseUserAdmin):
-    list_display = ('username', 'email', 'is_student', 'is_teacher', 'is_admin')
-    list_filter = ('is_student', 'is_teacher', 'is_admin')
